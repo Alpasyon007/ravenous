a9961c5d410b39ca2ecff24b19c8d08a1de39b3d
Alpasyon007 <alpasyon07@gmail.com>
2017-11-19 21:20:49 +0200

    Start of Ravenous project

---
+++ src/components/business-list/business-list.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./business-list.css";
+import Business from '../business/business';
+
+class BusinessList extends React.Component{
+  render(){
+    return <div className="BusinessList">
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+</div>
+  }
+}
+
+export default BusinessList;
